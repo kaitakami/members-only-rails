dd47ffacc4976eefa8c477c9647d16c5dd256acb
Kai Takami <kaitakami@protonmail.com>
2022-11-21 12:21:21 +0900

    FInish post view with posts partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController
   before_action :authenticate_member!, except: %i[index show]
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_member.posts.build
+  end
+
+  def create
+    @post = current_member.posts.build(user_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:post).permit(:title, :body, :member_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
-
+  get "dashboard/profile"
   resources :posts, only: %i[new create index show]
 end
